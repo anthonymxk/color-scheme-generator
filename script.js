34513c295d0d082fb937eff8a4428cfbfabad3ef
anthonymxk <anthonyxkeller@gmail.com>
2022-04-13 20:09:18 -0500

    Change design and add hex conversion Switched the design of the page and added a table in the lower left that allows for copying and pasting. The table also features the hex conversion.

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const box = document.getElementById('box')
 button.addEventListener('click', addDiv)
 
 function randomColor() {
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    var randomColor = "rgb(" + x + "," + y + "," + z + ")";
+    const x = Math.floor(Math.random() * 256);
+    const y = Math.floor(Math.random() * 256);
+    const z = Math.floor(Math.random() * 256);
+    const randomColor = "rgb(" + x + "," + y + "," + z + ")";
     return randomColor
     }
+
+    const rgb2hex = (rgb) => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
 
 function addDiv() {
     //generate color
@@ -36,4 +38,10 @@ function addDiv() {
     //color five
     document.getElementById('five').style.backgroundColor = `${colorFive}`
     document.getElementById('valueFive').textContent = `${colorFive}`
-}
+    //assign values to copy paste
+    document.getElementById('copypasteRGBValue').innerHTML = `${colorOne}<br>${colorTwo}<br>${colorThree}<br>${colorFour}<br>${colorFive}`
+    document.getElementById('copypaste').style.opacity = '1'
+    document.getElementById('copypasteHexValue').innerHTML = `${rgb2hex(colorOne)}<br>${rgb2hex(colorTwo)}<br>${rgb2hex(colorThree)}<br>${rgb2hex(colorFour)}<br>${rgb2hex(colorFive)}`
+    document.getElementById('copypasteHex').style.opacity = '1'
+  //  console.log(rgb2hex(colorOne))
+}   
