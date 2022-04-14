7b4bddd3748552fab966db2b5ea28f7cce3ba4c5
anthonymxk <anthonyxkeller@gmail.com>
2022-04-13 20:43:04 -0500

    Clean up code and styles

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const button = document.getElementById('button')
 const one = document.getElementById('one')
 const box = document.getElementById('box')
 
-button.addEventListener('click', addDiv)
+//when button is clicked, create palette
+button.addEventListener('click', createPalette)
+
 
 function randomColor() {
     const x = Math.floor(Math.random() * 256);
@@ -12,9 +14,10 @@ function randomColor() {
     return randomColor
     }
 
-    const rgb2hex = (rgb) => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
+    //arrow function to convert rgb to hex value
+const rgb2hex = (rgb) => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
 
-function addDiv() {
+function createPalette() {
     //generate color
     colorOne = randomColor()
     colorTwo = randomColor()
@@ -26,22 +29,29 @@ function addDiv() {
     //color one
     document.getElementById('one').style.backgroundColor = `${colorOne}`
     document.getElementById('valueOne').textContent = `${colorOne}`
+    document.getElementById('valueOneHex').textContent = `${rgb2hex(colorOne)}`
+
     //color two
     document.getElementById('two').style.backgroundColor = `${colorTwo}`
     document.getElementById('valueTwo').textContent = `${colorTwo}`
+    document.getElementById('valueTwoHex').textContent = `${rgb2hex(colorTwo)}`
     //color three
     document.getElementById('three').style.backgroundColor = `${colorThree}`
     document.getElementById('valueThree').textContent = `${colorThree}`
+    document.getElementById('valueThreeHex').textContent = `${rgb2hex(colorThree)}`
     //color four
     document.getElementById('four').style.backgroundColor = `${colorFour}`
     document.getElementById('valueFour').textContent = `${colorFour}`
+    document.getElementById('valueFourHex').textContent = `${rgb2hex(colorFour)}`
+
     //color five
     document.getElementById('five').style.backgroundColor = `${colorFive}`
     document.getElementById('valueFive').textContent = `${colorFive}`
+    document.getElementById('valueFiveHex').textContent = `${rgb2hex(colorFive)}`
+
     //assign values to copy paste
     document.getElementById('copypasteRGBValue').innerHTML = `${colorOne}<br>${colorTwo}<br>${colorThree}<br>${colorFour}<br>${colorFive}`
     document.getElementById('copypaste').style.opacity = '1'
     document.getElementById('copypasteHexValue').innerHTML = `${rgb2hex(colorOne)}<br>${rgb2hex(colorTwo)}<br>${rgb2hex(colorThree)}<br>${rgb2hex(colorFour)}<br>${rgb2hex(colorFive)}`
     document.getElementById('copypasteHex').style.opacity = '1'
-  //  console.log(rgb2hex(colorOne))
 }   
